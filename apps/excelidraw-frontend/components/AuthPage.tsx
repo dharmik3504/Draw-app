0cad851aacc3b3c451af304803c34a0c42a2a7c3
dharmik3504 <Heavynotebook@gmail.com>
2025-03-30 15:09:04 +0530

    forntwnd ui

---
+++ apps/excelidraw-frontend/components/AuthPage.tsx
@@ -0,0 +1,20 @@
+"use client";
+export function AuthPage({ isSignin }: { isSignin: boolean }) {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="p-6 m-2 bg-white rounded text-black">
+        <div className="p-2">
+          <input type="text" placeholder="Email"></input>
+        </div>
+        <div className="p-2">
+          <input type="password" placeholder="password"></input>
+        </div>
+        <div className="pt-2">
+          <button className="bg-red-200">
+            {isSignin ? "Sign In" : "Sign up"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
