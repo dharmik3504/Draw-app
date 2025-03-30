de230416b0b5c0ac92dd7221655b08a9e12a60b7
dharmik3504 <Heavynotebook@gmail.com>
2025-03-30 16:49:38 +0530

    added canvas logic in different file

--- apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Drawinit } from "@/app/draw";
 import { useEffect, useRef } from "react";
 
 export default function Canvas() {
@@ -6,38 +7,12 @@ export default function Canvas() {
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      let clicked = false;
-      let startX = 0;
-      let startY = 0;
 
-      canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        startX = e.clientX;
-        startY = e.clientY;
-        // ctx?.strokeRect(e.clientX, e.clientY, 100, 100);
-      });
-      canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-
-        console.log(e.clientX);
-        console.log(e.clientY);
-        // ctx?.strokeRect(e.clientX, e.clientY, 100, 100);
-      });
-      canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-          const width = e.clientX - startX;
-          const height = e.clientY - startY;
-
-          ctx?.clearRect(0, 0, canvas.width, canvas.height);
-          ctx?.strokeRect(startX, startY, width, height);
-        }
-        // ctx?.strokeRect(e.clientX, e.clientY, 100, 100);
-      });
+      Drawinit(canvas);
     }
   }, [canvasRef]);
   return (
-    <canvas ref={canvasRef} width={500} height={500}>
+    <canvas ref={canvasRef} width={"1080"} height={"1000"}>
       {" "}
     </canvas>
   );
